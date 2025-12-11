0b8616fbd9ed126c3bc42aa4a3df656c9c433ade
Raneem kashkoul <raneemkashkoul@gmail.com>
2025-12-11 17:19:36 +0300

    .

--- person/serializer.py
+++ person/serializer.py
@@ -1,50 +1,50 @@
-# from rest_framework import serializers
-# from .models import Person
-
-# class PersonSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Person
-#         fields = ['id', 'name', 'password', 'mobile', 'role']
-#         extra_kwargs = {
-#             'password': {'write_only': True}
-#         }
-
-#     def create(self, validated_data):
-#         password = validated_data.pop('password')     # اسحب الباسورد
-#         user = Person(**validated_data)               # أنشئ المستخدم
-#         user.set_password(password)                   # 🔥 شفر الباسورد هنا
-#         user.save()
-#         return user\
 from rest_framework import serializers
 from .models import Person
 
 class PersonSerializer(serializers.ModelSerializer):
-    action = serializers.CharField(write_only=True)  # 'register' أو 'login'
-
     class Meta:
         model = Person
-        fields = ['id', 'name', 'password', 'mobile', 'role', 'action']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate(self, data):
-        action = data.get('action')
-        name = data.get('name')
-        password = data.get('password')
-
-        if action == 'login':
-            try:
-                person = Person.objects.get(name=name)
-            except Person.DoesNotExist:
-                raise serializers.ValidationError("Invalid name or password")
-
-            if person.password != password:
-                raise serializers.ValidationError("Invalid name or password")
-
-            data['person'] = person
-
-        return data
+        fields = ['id', 'name', 'password', 'mobile', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        # إذا التسجيل
-        validated_data.pop('action', None)
-        return Person.objects.create(**validated_data)
+        password = validated_data.pop('password')     # اسحب الباسورد
+        user = Person(**validated_data)               # أنشئ المستخدم
+        user.set_password(password)                   # 🔥 شفر الباسورد هنا
+        user.save()
+        return user
+# from rest_framework import serializers
+# from .models import Person
+
+# class PersonSerializer(serializers.ModelSerializer):
+#     action = serializers.CharField(write_only=True)  # 'register' أو 'login'
+
+#     class Meta:
+#         model = Person
+#         fields = ['id', 'name', 'password', 'mobile', 'role', 'action']
+#         extra_kwargs = {'password': {'write_only': True}}
+
+#     def validate(self, data):
+#         action = data.get('action')
+#         name = data.get('name')
+#         password = data.get('password')
+
+#         if action == 'login':
+#             try:
+#                 person = Person.objects.get(name=name)
+#             except Person.DoesNotExist:
+#                 raise serializers.ValidationError("Invalid name or password")
+
+#             if person.password != password:
+#                 raise serializers.ValidationError("Invalid name or password")
+
+#             data['person'] = person
+
+#         return data
+
+#     def create(self, validated_data):
+#         # إذا التسجيل
+#         validated_data.pop('action', None)
+#         return Person.objects.create(**validated_data)
--- person/url.py
+++ person/url.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RegisterView, LoginView
-from .views import PersonAuthView
-# urlpatterns = [
-#     path('register/', RegisterView.as_view(), name='register'),
-#     path('login/', LoginView.as_view(), name='login'),
-# ]
+# from .views import PersonAuthView
 urlpatterns = [
-    path('auth/', PersonAuthView.as_view(), name='person-auth'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
+# urlpatterns = [
+#     path('auth/', PersonAuthView.as_view(), name='person-auth'),
+# ]
+ 
  
